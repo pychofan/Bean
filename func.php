73d4176bd4ed44eafbd4318669cecf454207614c
Pychofan <35198199+pychofan@users.noreply.github.com>
2018-05-31 04:59:33 +0200

    changed func.php

--- func.php
+++ func.php
@@ -1,8 +1,6 @@
 <?php
 $time = time() ;
-$menge = $_POST["menge"] ;
 include "sql.php" ;
-$ein = $_POST['id'] ;
 $s_auto = ("SELECT menge FROM pd") ;
 $r_auto = $db->query($s_auto) ->fetch();
 var_dump($r_auto);
