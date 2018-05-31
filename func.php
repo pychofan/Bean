6c0a5a83e97897a49ba2ab86259d323d000066aa
Pychofan <35198199+pychofan@users.noreply.github.com>
2018-05-31 05:01:58 +0200

    Changed funx.php again

--- func.php
+++ func.php
@@ -1,7 +1,7 @@
 <?php
 $time = time() ;
 include "sql.php" ;
-$s_auto = ("SELECT menge FROM pd") ;
+$s_auto = ("SELECT * FROM pd") ;
 $r_auto = $db->query($s_auto) ->fetch();
 var_dump($r_auto);
 ?>
