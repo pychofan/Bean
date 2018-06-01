162e16fb46688ab102beb04b13702458fe4f6bd3
Pychofan <35198199+pychofan@users.noreply.github.com>
2018-06-01 23:25:37 +0200

    Func.php Edit01

--- func.php
+++ func.php
@@ -1,7 +1,7 @@
 <?php
 $time = time() ;
 include "sql.php" ;
-$s_auto = ("SELECT * FROM pd") ;
+$s_auto = ALL ("SELECT * FROM pd") ;
 $r_auto = $db->query($s_auto) ->fetch();
 var_dump($r_auto);
 ?>
